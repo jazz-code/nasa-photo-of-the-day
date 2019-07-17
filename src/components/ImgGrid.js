5fae5776d1377a08ce841dcf42c56822303e242a
jazz-code <ren.meta@gmail.com>
2019-07-17 15:40:14 -0400

    export function, set useState to empty array, JSX div structure

--- src/components/ImgGrid.js
+++ src/components/ImgGrid.js
@@ -1,3 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ImgCard from "./ImgCard";
+
+export default function ImgGrid() {
+  const [data, setData] = useState([]);
+
+  return <div className="img-grid" />;
+}
