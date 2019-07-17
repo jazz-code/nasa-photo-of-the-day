38b48ccb601f8fee15671dd6d5599123c0839178
jazz-code <ren.meta@gmail.com>
2019-07-17 15:48:09 -0400

    useEffect with axios response to grab IMG

--- src/components/ImgGrid.js
+++ src/components/ImgGrid.js
@@ -5,5 +5,17 @@ import ImgCard from "./ImgCard";
 export default function ImgGrid() {
   const [data, setData] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15`
+      )
+      .then(res => {
+        console.log(res);
+        const nasaIMG = res.data.url;
+        setData(nasaIMG);
+      });
+  }, []);
+
   return <div className="img-grid" />;
 }
